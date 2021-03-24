c88900c3b47f29b9c36f84ce3da6d9ae9e280fe2
Malvika Oke <malvika.oke@gmail.com>
2021-03-24 19:46:45 +0000

    initial commit

---
+++ PycharmProjects/Tester/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+df = pd.read_csv(r'C:\Users\okeud\Documents\Python Data\Future50_rankings.csv.csv')
+print(df)
+
+df = pd.read_csv(r'C:\Users\okeud\Documents\Python Data\Independence100_ranking.csv.csv')
+print(df)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
